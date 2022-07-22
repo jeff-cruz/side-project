ca68f649d51e93ba458860d962a3a01f10db530c
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-22 23:29:36 +0000

    Retrieved Pokemon Data with Pagination

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
-import Home from './pages/home';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './components/pokemonlist';
+import Pagination from './components/pagination';
+import axios from 'axios';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [prevPage, setPrevPage] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPage, {
+      // eslint-disable-next-line no-return-assign
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
+      .then(res => {
+        setLoading(false);
+        setNextPage(res.data.next);
+        setPrevPage(res.data.previous);
+        setPokemon(res.data.results.map(p => p.name));
+      });
+
+    return () => cancel();
+  }, [currentPage]);
+
+  function goToNextPage() {
+    setCurrentPage(nextPage);
   }
+
+  function goToPrevPage() {
+    setCurrentPage(prevPage);
+  }
+
+  if (loading) return 'Loading...';
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPage ? goToNextPage : null}
+        goToPrevPage={prevPage ? goToPrevPage : null}
+      />
+    </>
+  );
 }
+
+export default App;
